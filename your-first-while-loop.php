92c163f48a98cd28b873ee0ac36cd00ba8512f7a
Your Name Here <amyr@amyrhaq.com>
2013-09-26 09:57:18 -0700

    completed lesson 3/7

---
+++ your-first-while-loop.php
@@ -0,0 +1,17 @@
+<!-- Write the code for your own while loop below the comment inside the php tags.  -->
+<html>
+<head>
+	<title>Your first while loop</title>
+</head>
+<body>
+<?php
+$loopCond = true;
+while ($loopCond)
+{
+	echo "<br>This loop is running</br>";
+	$loopCond = false;
+}
+	echo "The loop has stopped";
+?>
+</body>
+</html>
